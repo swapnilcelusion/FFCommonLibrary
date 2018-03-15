9ad09b0a54cd2edf836721bf42a99ae626c7c670
swapnil nandgave <swapnil.nandgave@celusion.com>
2018-03-15 17:25:37 +0530

    Changed Specs

---
+++ Example/Pods/Target Support Files/FFCommonLibrary/FFCommonLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FFCommonLibrary : NSObject
+@end
+@implementation PodsDummy_FFCommonLibrary
+@end
---
+++ HellowWorldSample.swift
@@ -0,0 +1,16 @@
+//
+//  HellowWorldSample.swift
+//  FFCommonLibrary
+//
+//  Created by Celusion Technologies on 15/03/18.
+//
+
+import Foundation
+
+public class HellowWorldSample {
+    
+    public func logit() {
+        print("POD HAS IMPLEMENTED")
+    }
+    
+}
